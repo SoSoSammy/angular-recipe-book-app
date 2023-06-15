7eed07ef1cfb0cca5b808cdb993a5920993f1268
SoSoSammy <86075091+SoSoSammy@users.noreply.github.com>
2023-06-15 12:00:32 -0600

    Create recipes array using Recipe class

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'Matcha Green Tea Cake',
+      'A delicious matcha green tea cake!',
+      'https://cf.foodista.com/content/fp/7cafpt65esowsd3f.jpg'
+    ),
+    new Recipe(
+      'Matcha Green Tea Cake',
+      'A delicious matcha green tea cake!',
+      'https://cf.foodista.com/content/fp/7cafpt65esowsd3f.jpg'
+    ),
+  ];
 }
