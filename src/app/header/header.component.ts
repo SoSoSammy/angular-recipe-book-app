f6996842883561a73c3f6eb0eb81a6984796970d
SoSoSammy <86075091+SoSoSammy@users.noreply.github.com>
2023-06-16 15:57:23 -0600

    Create featureSelected event

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
+  @Output() featureSelected = new EventEmitter<string>(); // Just need type to be string, not object
   collapsed = true;
+
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
+  }
 }
