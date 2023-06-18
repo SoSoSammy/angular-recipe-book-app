fdaf4720bf0b638acbc5eb106d61cfb5a4e6f031
SoSoSammy <86075091+SoSoSammy@users.noreply.github.com>
2023-06-17 18:56:57 -0600

    Emit recipeSelected event

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
   styleUrls: ['./recipe-item.component.css'],
 })
-export class RecipeItemComponent {}
+export class RecipeItemComponent {
+  // Make recipe property accessible from outside the component
+  @Input() recipe: Recipe;
+  // Make recipeSelected event listenable from outside the component
+  @Output() recipeSelected = new EventEmitter<void>(); // void because we don't pass in any data
+  
+  // Emit an event when the recipe is selected
+  onSelected(recipe: Recipe) {
+    this.recipeSelected.emit();
+  }
+}
