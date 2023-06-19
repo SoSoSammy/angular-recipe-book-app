0bcd4a925e6882ecca62bddc680c530f33b768f1
SoSoSammy <86075091+SoSoSammy@users.noreply.github.com>
2023-06-19 15:29:34 -0600

    Use RecipeService to create recipes array

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,29 +1,19 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css'],
 })
-export class RecipeListComponent {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[];
 
-  recipes: Recipe[] = [
-    new Recipe(
-      'Matcha Green Tea Cake',
-      'A delicious matcha green tea cake!',
-      'https://cf.foodista.com/content/fp/7cafpt65esowsd3f.jpg'
-    ),
-    new Recipe(
-      'Another Matcha Green Tea Cake',
-      'Yet another delicious matcha green tea cake!',
-      'https://cf.foodista.com/content/fp/7cafpt65esowsd3f.jpg'
-    ),
-  ];
+  constructor(private recipeService: RecipeService) {}
 
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 }
