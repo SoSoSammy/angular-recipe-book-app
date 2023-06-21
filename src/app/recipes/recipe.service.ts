09b0eb23059468e9198b6f68f44fcbe5dfcab6f5
Samantha Vazquez <86075091+SoSoSammy@users.noreply.github.com>
2023-06-21 14:52:46 -0600

    Add getRecipe method

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,4 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
@@ -6,8 +6,6 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>();
-
   private recipes: Recipe[] = [
     new Recipe(
       'Matcha Green Tea Cake',
@@ -33,6 +31,10 @@ export class RecipeService {
   ];
 
   constructor(private shoppingListService: ShoppingListService) {}
+
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
 
   getRecipes() {
     return this.recipes.slice(); // return a copy of the array
