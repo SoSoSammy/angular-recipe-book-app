668baa8ef7e8f5a50737521ce4b7fcbd4cbfadca
Samantha Vazquez <86075091+SoSoSammy@users.noreply.github.com>
2023-06-23 10:09:48 -0600

    Add methods for working with recipe form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,14 +12,97 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false;
+  recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private recipeService: RecipeService
+  ) {}
 
   ngOnInit() {
+    // Subscribe to changes in URL parameters
     this.route.params.subscribe((params: Params) => {
       this.id = +params.id;
       // Decide if we are in edit mode based on if id URL parameter is present
       this.editMode = params.id != null;
+      this.initForm();
     });
+  }
+
+  private initForm() {
+    // Set default recipe values
+    let recipeName = '';
+    let recipeImagePath = '';
+    let recipeDescription = '';
+    let recipeIngredients = new FormArray([]);
+
+    // If in edit mode, set default recipe values to recipe passed in from URL
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipe(this.id);
+      recipeName = recipe.name;
+      recipeImagePath = recipe.imagePath;
+      recipeDescription = recipe.description;
+
+      // If the recipe has ingredients, create a new form group for each ingredient
+      if (recipe.ingredients) {
+        recipe.ingredients.forEach(ingredient =>
+          recipeIngredients.push(
+            // name of this FormGroup is its index in the FormArray
+            new FormGroup({
+              name: new FormControl(ingredient.name, Validators.required),
+              amount: new FormControl(ingredient.amount, Validators.required),
+              // If wanted to do number validation, do:
+              // amount: new FormControl(ingredient.amount, [
+              //   Validators.required,
+              //   Validators.pattern(/^[1-9]+[0-9]*$/),
+              // ]),
+            })
+          )
+        );
+      }
+    }
+
+    // Fill out the form controls with the default value of '' or the recipe's values
+    this.recipeForm = new FormGroup({
+      name: new FormControl(recipeName, Validators.required),
+      imagePath: new FormControl(recipeImagePath, Validators.required),
+      description: new FormControl(recipeDescription, Validators.required),
+      ingredients: recipeIngredients,
+    });
+  }
+
+  // Get the ingredient form controls
+  get controls() {
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
+  }
+
+  // Add new ingredient form group to form
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        name: new FormControl(null, Validators.required),
+        amount: new FormControl(null, Validators.required),
+        // If wanted to do number validation, do:
+        // amount: new FormControl(null, [
+        //   Validators.required,
+        //   Validators.pattern(/^[1-9]+[0-9]*$/),
+        // ]),
+      })
+    );
+  }
+
+  // When the form is submitted
+  onSubmit() {
+    // const newRecipe = new Recipe(
+    //   this.recipeForm.value.name,
+    //   this.recipeForm.value.description,
+    //   this.recipeForm.value.imagePath,
+    //   this.recipeForm.value.ingredients
+    // );
+
+    // If we are in edit mode, update existing recipe, if not, add new recipe
+    if (this.editMode)
+      this.recipeService.updateRecipe(this.id, this.recipeForm.value);
+    else this.recipeService.addRecipe(this.recipeForm.value);
   }
 }
