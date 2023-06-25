7d466339d46ddf5c15a2148eccde4b6e786fd925
SoSoSammy <86075091+SoSoSammy@users.noreply.github.com>
2023-06-25 10:04:33 -0600

    Add setRecipe method

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,38 +9,44 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Matcha Green Tea Cake',
-      'A delicious matcha green tea cake!',
-      'https://cf.foodista.com/content/fp/7cafpt65esowsd3f.jpg',
-      [
-        new Ingredient('Eggs', 2),
-        new Ingredient('All-purpose flour', '3/4 cup'),
-        new Ingredient('Matcha green tea powder', '1 1/2 tbsp'),
-        new Ingredient('Raspberries', '1/2 cup'),
-      ]
-    ),
-    new Recipe(
-      'Sourdough Bread',
-      'Easy to make sourdough bread!',
-      'https://th.bing.com/th/id/OIP.1GvKGEtfYmOpxwi-zj_vpQHaE6?pid=ImgDet&rs=1',
-      [
-        new Ingredient('Whole-wheat flour', '6 cups'),
-        new Ingredient('Sourdough starter', '1/4 cup'),
-        new Ingredient('Salt', '1 tsp'),
-      ]
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Matcha Green Tea Cake',
+  //     'A delicious matcha green tea cake!',
+  //     'https://cf.foodista.com/content/fp/7cafpt65esowsd3f.jpg',
+  //     [
+  //       new Ingredient('Eggs', 2),
+  //       new Ingredient('All-purpose flour', '3/4 cup'),
+  //       new Ingredient('Matcha green tea powder', '1 1/2 tbsp'),
+  //       new Ingredient('Raspberries', '1/2 cup'),
+  //     ]
+  //   ),
+  //   new Recipe(
+  //     'Sourdough Bread',
+  //     'Easy to make sourdough bread!',
+  //     'https://th.bing.com/th/id/OIP.1GvKGEtfYmOpxwi-zj_vpQHaE6?pid=ImgDet&rs=1',
+  //     [
+  //       new Ingredient('Whole-wheat flour', '6 cups'),
+  //       new Ingredient('Sourdough starter', '1/4 cup'),
+  //       new Ingredient('Salt', '1 tsp'),
+  //     ]
+  //   ),
+  // ];
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {}
 
-  getRecipe(index: number) {
-    return this.recipes[index];
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
   }
 
   getRecipes() {
     return this.recipes.slice(); // return a copy of the array
+  }
+
+  getRecipe(index: number) {
+    return this.recipes[index];
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
