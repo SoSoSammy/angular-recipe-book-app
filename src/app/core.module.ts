1bc99552bfb94926e597da031c8f344d3bc536ee
Samantha Vazquez <86075091+SoSoSammy@users.noreply.github.com>
2023-06-26 11:30:33 -0600

    Create core.module.ts

---
+++ src/app/core.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
+import { RecipeService } from './recipes/recipe.service';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
+
+@NgModule({
+  // don't need to export services since they are automatically injected at the root level
+  // providers has all services (can also use @Injectable{providedIn: 'root'} for the service)
+  providers: [
+    ShoppingListService,
+    RecipeService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true,
+    },
+  ],
+})
+export class CoreModule {} // core module is for all services
