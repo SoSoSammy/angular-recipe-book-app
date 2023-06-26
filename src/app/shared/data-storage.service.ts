8b099ef8d57bc476cb95c6f85cd24650af9a5718
Samantha Vazquez <86075091+SoSoSammy@users.noreply.github.com>
2023-06-26 15:00:59 -0600

    Add user id to recipe http requests

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { exhaustMap, map, take, tap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { map, tap, filter } from 'rxjs/operators';
 
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
@@ -16,21 +16,29 @@ export class DataStorageService {
 
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
+
+    // Get unique user id to put in URL for database
+    const userId = this.getUserId();
+
     // Override all existing recipes
     this.http
       .put(
-        'https://angular-recipe-book-app-f5a71-default-rtdb.firebaseio.com/recipes.json',
+        `https://angular-recipe-book-app-f5a71-default-rtdb.firebaseio.com/${userId}.json`,
         recipes
       )
       .subscribe(response => console.log(response));
   }
 
   fetchRecipes() {
+    // Get unique user id to put in URL for database
+    const userId = this.getUserId();
+
     return this.http
       .get<Recipe[]>(
-        'https://angular-recipe-book-app-f5a71-default-rtdb.firebaseio.com/recipes.json'
+        `https://angular-recipe-book-app-f5a71-default-rtdb.firebaseio.com/${userId}.json`
       )
       .pipe(
+        filter(recipes => recipes != undefined), // only continue execution if recipes exists
         map(recipes => {
           // If the recipe has no ingredients, set ingredients to an empty array
           return recipes.map(recipe => {
@@ -43,5 +51,12 @@ export class DataStorageService {
         // Execute code without altering data in observable
         tap(recipes => this.recipeService.setRecipes(recipes))
       );
+  }
+
+  private getUserId() {
+    let userId: string;
+    this.authService.user.subscribe(user => (userId = user.id)).unsubscribe();
+    console.log(userId);
+    return userId;
   }
 }
