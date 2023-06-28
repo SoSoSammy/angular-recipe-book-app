27dd6243392d5e7edcac2a6a066f1c7dd5479016
Samantha Vazquez <86075091+SoSoSammy@users.noreply.github.com>
2023-06-28 10:32:34 -0600

    Remove ShoppingListService

--- src/app/core.module.ts
+++ src/app/core.module.ts
@@ -3,14 +3,11 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { RecipeService } from './recipes/recipe.service';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { LoggingService } from './logging.service';
 
 @NgModule({
   // don't need to export services since they are automatically injected at the root level
   // providers has all services (can also use @Injectable{providedIn: 'root'} for the service)
   providers: [
-    ShoppingListService,
     RecipeService,
     {
       provide: HTTP_INTERCEPTORS,
