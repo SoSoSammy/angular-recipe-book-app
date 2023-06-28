3fd210636fcd9dcf1445eff21cf41f924d0d83ac
Samantha Vazquez <86075091+SoSoSammy@users.noreply.github.com>
2023-06-28 10:34:35 -0600

    Add cases for starting and stopping editing

--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -24,45 +24,75 @@ export function shoppingListReducer(
   action: ShoppingListActions.ShoppingListActions
 ) {
   switch (action.type) {
+    //////////////////////////
+    // Add new ingredient
     case ShoppingListActions.ADD_INGREDIENT: // convention is all caps
       return {
         ...state, // copy old state
         ingredients: [...state.ingredients, action.payload],
       };
 
+    // Add multiple ingredients
     case ShoppingListActions.ADD_INGREDIENTS:
       return {
         ...state,
         ingredients: [...state.ingredients, ...action.payload],
       };
 
+    //////////////////////////
+    // Update an ingredient
     case ShoppingListActions.UPDATE_INGREDIENT:
       // Get the ingredient with the specified index
-      const ingredient = state.ingredients[action.payload.index];
+      const ingredient = state.ingredients[state.editedIngredientIndex];
       // Create the updated ingredient based on old ingredient data + new data
       const updatedIngredient = {
         ...ingredient,
-        ...action.payload.ingredient,
+        ...action.payload,
       };
       // Copy the old ingredients
       const updatedIngredients = [...state.ingredients];
       // Replace existing ingredient with specified index with updated ingredient
-      updatedIngredients[action.payload.index] = updatedIngredient;
+      updatedIngredients[state.editedIngredientIndex] = updatedIngredient;
 
       return {
         ...state,
         ingredients: updatedIngredients,
+        editedIngredient: null,
+        editedIngredientIndex: -1,
       };
 
+    //////////////////////////
+    // Delete an ingredient
     case ShoppingListActions.DELETE_INGREDIENT:
       return {
         ...state,
         // Filter through ingredients and return updated ingredients without deleted ingredient
         ingredients: state.ingredients.filter((ig, igIndex) => {
           // Don't add the ingredient if the index is the same as the specified deleted item index
-          return igIndex !== action.payload;
+          return igIndex !== state.editedIngredientIndex;
         }),
+        editedIngredient: null,
+        editedIngredientIndex: -1,
       };
+
+    //////////////////////////
+    // Start editing
+    case ShoppingListActions.START_EDIT:
+      return {
+        ...state,
+        editedIngredientIndex: action.payload,
+        editedIngredient: { ...state.ingredients[action.payload] },
+      };
+
+    //////////////////////////
+    // Stop editing
+    case ShoppingListActions.STOP_EDIT:
+      return {
+        ...state,
+        editedIngredient: null,
+        editedIngredientIndex: -1,
+      };
+
     default:
       return state;
   }
