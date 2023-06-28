2016340fe3347d75cb85dc44a2606fc16191de8b
landonkb <86075091+SoSoSammy@users.noreply.github.com>
2023-06-28 15:19:15 -0600

    Use auth effects for login

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnDestroy, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Observable, Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
 
 import { AuthService, AuthResponseData } from './auth.service';
 import { AlertComponent } from '../shared/alert/alert.component';
 import { PlaceholderDirective } from '../shared/placeholder/placeholder.directive';
+import * as fromApp from '../store/app.reducer';
+import * as AuthActions from '../auth/store/auth.actions';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
 })
-export class AuthComponent implements OnDestroy {
+export class AuthComponent implements OnInit, OnDestroy {
   isLoginMode = true;
   isLoading = false;
   error: string = null;
@@ -21,9 +24,18 @@ export class AuthComponent implements OnDestroy {
 
   constructor(
     private authService: AuthService,
-    private router: Router
-  ) // private componentFactoryResolver: ComponentFactoryResolver
-  {}
+    private router: Router,
+    // private componentFactoryResolver: ComponentFactoryResolver
+    private store: Store<fromApp.AppState>
+  ) {}
+
+  ngOnInit() {
+    this.store.select('auth').subscribe(authState => {
+      this.isLoading = authState.loading;
+      this.error = authState.authError;
+      if (this.error) this.showErrorAlert(this.error);
+    });
+  }
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode; // reverse value of isLoginMode
@@ -39,24 +51,28 @@ export class AuthComponent implements OnDestroy {
 
     this.isLoading = true;
     // Login
-    if (this.isLoginMode) authObs = this.authService.login(email, password);
+    // if (this.isLoginMode) authObs = this.authService.login(email, password);
+    if (this.isLoginMode)
+      this.store.dispatch(
+        new AuthActions.LoginStart({ email: email, password: password })
+      );
     // Signup
     else authObs = this.authService.signup(email, password);
 
     // Subscribe to the observable returned by login or signup and handle errors
-    authObs.subscribe(
-      resData => {
-        console.log(resData);
-        this.isLoading = false;
-        this.router.navigate(['/recipes']);
-      },
-      errorMessage => {
-        console.log(errorMessage);
-        this.error = errorMessage;
-        this.showErrorAlert(errorMessage);
-        this.isLoading = false;
-      }
-    );
+    // authObs.subscribe(
+    //   resData => {
+    //     console.log(resData);
+    //     this.isLoading = false;
+    //     this.router.navigate(['/recipes']);
+    //   },
+    //   errorMessage => {
+    //     console.log(errorMessage);
+    //     this.error = errorMessage;
+    //     this.showErrorAlert(errorMessage);
+    //     this.isLoading = false;
+    //   }
+    // );
 
     form.reset();
   }
