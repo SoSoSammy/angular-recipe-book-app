fed92f632450da4253c9447f76867e26f74f514e
Samantha Vazquez <86075091+SoSoSammy@users.noreply.github.com>
2023-06-28 09:28:32 -0600

    Add shoppingListReducer to StoreModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
-import { LoggingService } from './logging.service';
-import { StoreModule } from '@ngrx/store';
+import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer';
 
 @NgModule({
   // declarations has all components, directives, and pipes
@@ -21,7 +21,7 @@ import { StoreModule } from '@ngrx/store';
     AppRoutingModule,
     SharedModule,
     CoreModule,
-    StoreModule.forRoot({}, {}),
+    StoreModule.forRoot({ shoppingList: shoppingListReducer }),
   ],
   // defines which component is available right in the index.html file (typically only 1)
   bootstrap: [AppComponent],
