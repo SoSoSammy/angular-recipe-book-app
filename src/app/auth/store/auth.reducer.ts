8111b98b78b84323b1342bbed74f040ec1dfdb2f
Samantha Vazquez <86075091+SoSoSammy@users.noreply.github.com>
2023-06-28 11:35:58 -0600

    Create auth.reducer.ts

---
+++ src/app/auth/store/auth.reducer.ts
@@ -0,0 +1,42 @@
+import { User } from '../user.model';
+import * as AuthActions from './auth.actions';
+
+export interface State {
+  user: User;
+}
+
+const initialState: State = {
+  user: null,
+};
+
+export function authReducer(
+  state = initialState,
+  action: AuthActions.AuthActions
+) {
+  switch (action.type) {
+    //////////////////////////
+    // Login
+    case AuthActions.LOGIN:
+      const user = new User(
+        action.payload.email,
+        action.payload.userId,
+        action.payload.token,
+        action.payload.expirationDate
+      );
+      return {
+        ...state,
+        user: user,
+      };
+
+    //////////////////////////
+    // Logout
+    case AuthActions.LOGOUT:
+      return {
+        ...state,
+        user: null,
+      };
+
+    default:
+      return state;
+  }
+}
