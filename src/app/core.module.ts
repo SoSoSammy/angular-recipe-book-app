1c3334dd040a8b2c2f5c9bf950f3d6f7cb0601fa
Samantha Vazquez <86075091+SoSoSammy@users.noreply.github.com>
2023-06-29 14:16:07 -0600

    Remove RecipeService

--- src/app/core.module.ts
+++ src/app/core.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
-import { RecipeService } from './recipes/recipe.service';
 
 @NgModule({
   // don't need to export services since they are automatically injected at the root level
   // providers has all services (can also use @Injectable{providedIn: 'root'} for the service)
   providers: [
-    RecipeService,
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptorService,
