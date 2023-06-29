2e07795b8b716c5b14be0522c49df44afedaac1f
Samantha Vazquez <86075091+SoSoSammy@users.noreply.github.com>
2023-06-29 14:18:16 -0600

    Add getUserId method and pass id to fetch recipes

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -9,16 +9,12 @@ import { Actions, ofType } from '@ngrx/effects';
 import { map, of, switchMap, take } from 'rxjs';
 
 import { Recipe } from './recipe.model';
-import { DataStorageService } from '../shared/data-storage.service';
-import { RecipeService } from './recipe.service';
 import * as fromApp from '../store/app.reducer';
 import * as RecipeActions from '../recipes/store/recipe.actions';
 
 @Injectable({ providedIn: 'root' })
 export class RecipesResolverService implements Resolve<Recipe[]> {
   constructor(
-    private dataStorageService: DataStorageService,
-    private recipeService: RecipeService,
     private store: Store<fromApp.AppState>,
     private actions$: Actions
   ) {}
@@ -26,14 +22,14 @@ export class RecipesResolverService implements Resolve<Recipe[]> {
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     // the resolver automatically subscribes for us
     return this.store.select('recipes').pipe(
-      // take(1),
+      take(1),
       map(recipesState => {
         return recipesState.recipes;
       }),
       switchMap(recipes => {
         // If there are no recipes, fetch them
         if (recipes.length === 0) {
-          this.store.dispatch(new RecipeActions.FetchRecipes());
+          this.store.dispatch(new RecipeActions.FetchRecipes(this.getUserId()));
           return this.actions$.pipe(ofType(RecipeActions.SET_RECIPES), take(1));
         }
         // If we already have recipes, return them
@@ -42,5 +38,16 @@ export class RecipesResolverService implements Resolve<Recipe[]> {
         }
       })
     );
+  }
+
+  private getUserId() {
+    let userId: string;
+    this.store
+      .select('auth')
+      .pipe(map(authState => authState.user))
+      .subscribe(user => (userId = user.id))
+      .unsubscribe();
+    console.log(userId);
+    return userId;
   }
 }
