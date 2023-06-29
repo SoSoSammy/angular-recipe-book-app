7a2038025d1e3d2bf8a1d977b493ac94b44c886d
Samantha Vazquez <86075091+SoSoSammy@users.noreply.github.com>
2023-06-29 10:26:23 -0600

    Add recipes to state

--- src/app/store/app.reducer.ts
+++ src/app/store/app.reducer.ts
@@ -2,14 +2,17 @@ import { ActionReducerMap } from '@ngrx/store';
 
 import * as fromShoppingList from '../shopping-list/store/shopping-list.reducer'; // use from to describe imports from reducer and/or state
 import * as fromAuth from '../auth/store/auth.reducer';
+import * as fromRecipes from '../recipes/store/recipe.reducer';
 
 // Define overall AppState type
 export interface AppState {
   shoppingList: fromShoppingList.State;
   auth: fromAuth.State;
+  recipes: fromRecipes.State;
 }
 
 export const appReducer: ActionReducerMap<AppState> = {
   shoppingList: fromShoppingList.shoppingListReducer,
   auth: fromAuth.authReducer,
+  recipes: fromRecipes.recipeReducer,
 };
