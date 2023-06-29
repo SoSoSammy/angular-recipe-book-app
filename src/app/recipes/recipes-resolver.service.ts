87d4ff40ce281f819d1786d80488338e645cd4d2
Samantha Vazquez <86075091+SoSoSammy@users.noreply.github.com>
2023-06-29 12:29:16 -0600

    Don't override existing recipes in resolver

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -6,7 +6,7 @@ import {
 } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Actions, ofType } from '@ngrx/effects';
-import { take } from 'rxjs';
+import { map, of, switchMap, take } from 'rxjs';
 
 import { Recipe } from './recipe.model';
 import { DataStorageService } from '../shared/data-storage.service';
@@ -24,14 +24,23 @@ export class RecipesResolverService implements Resolve<Recipe[]> {
   ) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    const recipes = this.recipeService.getRecipes();
-
-    // When there are no recipes, fetch them
-    if (recipes.length === 0) {
-      // the resolver automatically subscribes for us
-      // return this.dataStorageService.fetchRecipes();
-      this.store.dispatch(new RecipeActions.FetchRecipes());
-      return this.actions$.pipe(ofType(RecipeActions.SET_RECIPES), take(1));
-    } else return recipes;
+    // the resolver automatically subscribes for us
+    return this.store.select('recipes').pipe(
+      // take(1),
+      map(recipesState => {
+        return recipesState.recipes;
+      }),
+      switchMap(recipes => {
+        // If there are no recipes, fetch them
+        if (recipes.length === 0) {
+          this.store.dispatch(new RecipeActions.FetchRecipes());
+          return this.actions$.pipe(ofType(RecipeActions.SET_RECIPES), take(1));
+        }
+        // If we already have recipes, return them
+        else {
+          return of(recipes);
+        }
+      })
+    );
   }
 }
